41794e760207e99009cf1cd2e5ce8d511aa154bb
Sebastian Krysmanski <noreply@manski.net>
2019-02-17 14:34:18 +0100

    Added sample C# project

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Calculator
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
